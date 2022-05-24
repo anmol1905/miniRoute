c9adbff98674d8315d5758c6d09f53068429ddf9
anmol1905 <anveeg.sinha@gmail.com>
2022-05-24 10:12:48 +0530

    custom middleware

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ![npm (scoped)](https://img.shields.io/npm/v/@anveeg/mini-route)
 [![install size](https://packagephobia.com/badge?p=@anveeg/mini-route)](https://bundle.com/result?p=@anveeg/mini-route)
-
+![npm bundle size (scoped)](https://img.shields.io/bundlephobia/min/@anveeg/mini-route)
 ## Install
 
 npm i @anveeg/mini-route
--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-var api_get= function (app) {
+var api_get = function (app, constant) {
   function respond(req, res, next) {
-      console.log(req.params)
+    constant[req.params.functionName](req, res, next)
+    res.json({"message": "success"})
   }; //function respond(req, res, next) {
 
-  // Routes
-  app.get('/:functionName/:payload?', respond);
-} 
+  function customMiddleware(req, res, next) {
+    let reqObj = JSON.parse(JSON.stringify(req.params.payload))
+    for (let i = 0; i < reqObj.middleware.length; i++) {
+      await constant[reqObj.middleware[i]](req, res, next);
+    }
+    res.json({"message": "success"})
+  }
 
-var api_post= function (app) {
+  // Routes
+  app.get('/:functionName/:payload?', customMiddleware, respond);
+}
+
+var api_post = function (app) {
   function post_handler(req, res, next) {
     console.log(req.params)
-  }; 
+  };
 
-  app.post('/:functionName/:payload?', post_handler);    
-}  
+  app.post('/:functionName/:payload?', post_handler);
+}
 
 module.exports = {
   api_get: api_get,
