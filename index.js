5e35440138a7595c9ab87dccb2d4229cdb44b8f7
anmol1905 <anveeg.sinha@gmail.com>
2022-05-26 11:52:51 +0530

    draft 1, gitignore, nodecache and linereader

--- index.js
+++ index.js
@@ -1,30 +1,57 @@
-var api_get = function (app, constant) {
-  function respond(req, res, next) {
-    constant[req.params.functionName](req, res, next)
-    res.json({"message": "success"})
-  }; //function respond(req, res, next) {
+const lineReader = require('line-reader');
+const NodeCache = require("node-cache");
+const myCache = new NodeCache();
 
-  function customMiddleware(req, res, next) {
-    let reqObj = JSON.parse(JSON.stringify(req.params.payload))
-    for (let i = 0; i < reqObj.middleware.length; i++) {
-      await constant[reqObj.middleware[i]](req, res, next);
-    }
-    res.json({"message": "success"})
+const api = async function (app, constant) {
+  let obj = {}
+  for (let i = 0; i < constant.length; i++) {
+    let lineCounter = 0
+    await lineReader.eachLine(constant[i], function (line) {
+      lineCounter++
+      if (lineCounter == 1) {
+        obj[line.replace(/(.*)=/, "").trim().split(' ')[0]] = constant[i]
+        myCache.set("controllers", obj);
+        return false; // stop reading
+      }
+    });
   }
 
-  // Routes
-  app.get('/:functionName/:payload?', customMiddleware, respond);
-}
-
-var api_post = function (app) {
-  function post_handler(req, res, next) {
-    console.log(req.params)
+  function controllerFunction(req, res, next) {
+    try {
+      let cacheObj = myCache.get("controllers")
+      let data = Object.keys(cacheObj).filter(key => key == req.params.functionName.split('.')[0])[0];
+      let controller = require(cacheObj[data])
+      controller[req.params.functionName.split('.')[1]](req, res, next)
+    } catch (err) {
+      console.error(err)
+    }
   };
 
-  app.post('/:functionName/:payload?', post_handler);
+  async function customMiddleware(req, res, next) {
+    try {
+      let cacheObj = myCache.get("controllers")
+      let reqObj = JSON.parse(req.params.payload)
+      
+      for (let i = 0; i < reqObj.middleware.length; i++) {
+        
+        let data = Object.keys(cacheObj).filter(key => key == reqObj.middleware[i].split('.')[0])[0];
+        let controller = require(cacheObj[data])
+       
+        await controller[reqObj.middleware[i].split('.')[1]](req, res, next);
+      }
+    } catch (err) {
+      console.log(err.message)
+    }
+    next()
+  }
+  // Routes
+  app.get('/:functionName/:payload?',customMiddleware, controllerFunction);
+  app.post('/:functionName/:payload?', customMiddleware, controllerFunction);
+  app.put('/:functionName/:payload?', customMiddleware, controllerFunction);
+  app.delete('/:functionName/:payload?', customMiddleware, controllerFunction);
+
 }
 
 module.exports = {
-  api_get: api_get,
-  api_post: api_post,
+  api: api,
 };
